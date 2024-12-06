6bc11c1a3ac13b173667b603f9ceaa61c2f08dfc
nour maayoufi <maayoufinour@gmail.com>
2024-12-06 14:31:49 +0100

    Inscription & Connexion set up

--- edu.iset.atelierspringboot/src/main/java/edu/iset/atelierspringboot/controllers/utilisateurController.java
+++ edu.iset.atelierspringboot/src/main/java/edu/iset/atelierspringboot/controllers/utilisateurController.java
@@ -1,12 +1,15 @@
 package edu.iset.atelierspringboot.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Utilisateur") //localhost :8080/Utilisateur 
+@RequestMapping("/Utilisateur") // localhost:8081/Utilisateur
 public class utilisateurController {
-	@GetMapping
-	
 
+    @GetMapping
+    public String getUtilisateur() {
+        return "Hello, Utilisateur!"; // You can return whatever data you need here
+    }
 }
